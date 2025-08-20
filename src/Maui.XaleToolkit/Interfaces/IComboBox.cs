6fdda76e8d030259fd11fefdb0ca67360d693d2c
axdelafuen <axel.dlf63@gmail.com>
2025-08-20 11:13:33 +0200

    move Event to dedicated folder

--- src/Maui.XaleToolkit/Interfaces/IComboBox.cs
+++ src/Maui.XaleToolkit/Interfaces/IComboBox.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections;
+﻿using Maui.XaleToolkit.Primitives;
+using System.Collections;
 
 namespace Maui.XaleToolkit.Interfaces
 {
@@ -46,29 +47,5 @@ namespace Maui.XaleToolkit.Interfaces
         /// Event fired when selection changes
         /// </summary>
         event EventHandler<ComboBoxSelectionChangedEventArgs> SelectionChanged;
-    }
-
-    /// <summary>
-    /// Event arguments for the SelectionChanged event of a <see cref="IComboBox"/> control.
-    /// </summary>
-    /// <param name="selectedItem">The selected item</param>
-    /// <param name="selectedIndex">The index of the selected item</param>
-    /// <param name="previousSelection">The previous selected item</param>
-    public class ComboBoxSelectionChangedEventArgs(object? selectedItem, int selectedIndex, object previousSelection) : EventArgs
-    {
-        /// <summary>
-        /// Gets the currently selected item.
-        /// </summary>
-        public object? SelectedItem { get; } = selectedItem;
-
-        /// <summary>
-        /// Gets the index of the currently selected item.
-        /// </summary>
-        public int SelectedIndex { get; } = selectedIndex;
-
-        /// <summary>
-        /// Gets the previously selected item.
-        /// </summary>
-        public object PreviousSelection { get; } = previousSelection;
     }
 }
